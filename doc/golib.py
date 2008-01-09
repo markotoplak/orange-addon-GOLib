9f46f589e8ac19322aa07392d940357ea87e64ec
ales <ales@abb54cfb-9aa5-4a4f-ad95-02929a718f71>
2008-01-09 09:47:51 +0000

    no message

--- doc/golib.py
+++ doc/golib.py
@@ -8,7 +8,7 @@ geneNames=[ "YBR085W", ## synonim for AAC3
 uniqueGeneNames = [go.geneMapper[name] for name in geneNames]
 print uniqueGeneNames #should print ["AAC3", "AAD10", "21S_rRNA_4"]
 
-#finding all term with the keywords "catabolic" and "process" in their names
+#finding all terms with the keywords "catabolic" and "process" in their names
 selectedTerms=filter(lambda term: "catabolic" in term.name and "process" in term.name, go.loadedGO.terms)
 for term in selectedTerms:
 	print term.id, term.name, term._def
@@ -21,3 +21,6 @@ res.sort(lambda (_1,(_2,a,_3)), (_4,(_5,b,_6)): cmp(a,b))
 for GOId,(genes, p, n) in res:
 	print GOId, p, genes
 
+terms=go.GOTermFinder(uniqueGeneNames)
+terms=go.filterByPValue(terms, 0.2)
+go.drawEnrichmentGraph(terms, "enrichment_graph.png", width=600)
